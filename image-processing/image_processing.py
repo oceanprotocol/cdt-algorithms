8231754b2099c42cf1a1f2f503884f170c05e8d0
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-09-25 13:32:23 +0300

    Added image-processing algorithm with multiple filters. for URL image.

---
+++ image-processing/image_processing.py
@@ -0,0 +1,39 @@
+import requests
+import sys
+
+from PIL import Image, ImageFilter
+from io import BytesIO
+
+def apply_filters(image_url, filter):
+    if not image_url:
+        print("Image URL is not provided.")
+        return
+    if not filter:
+        print("Filter is not provided.")
+        return
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+    filtered_img = None
+
+    # Apply filter
+    if filter == 'blur':
+        blurred_img = img.filter(ImageFilter.GaussianBlur(radius=5))
+        filtered_img = blurred_img
+    elif filter == 'grayscale':
+        grayscale_img = img.convert("L")
+        filtered_img = grayscale_img
+    elif filter == 'unsharp':
+        unsharp_img = img.filter(ImageFilter.UnsharpMask(radius=5))
+        filtered_img = unsharp_img
+    else:
+        print("Unknown filter.")
+    
+    img.save(filtered_img)
+
+if __name__ == "__main__":
+    # The URL for image must be public and accessible
+    image_url = sys.argv[1]
+    img_filter = sys.argv[2]
+    
+    apply_filters(image_url, filter=img_filter)
+    print("Filters applied and images saved successfully.")
