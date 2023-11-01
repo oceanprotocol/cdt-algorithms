8ccec85cb081f0a4af616b67784723f388fb47cf
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-11-01 12:35:46 +0200

    Styled tables.

--- generating_pdf_report/generating_pdf_report.py
+++ generating_pdf_report/generating_pdf_report.py
@@ -3,10 +3,16 @@ import os
 import json
 
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import Table
+from reportlab.platypus import Table, TableStyle
 from reportlab.lib import colors
 from reportlab.pdfgen import canvas
 
+LIST_TABLE_STYLE = TableStyle([
+    ('GRID', (1,1), (-1,-1), 0.25, colors.black),
+    ('FONTNAME', (0,0), (0,-1), 'Helvetica-Bold'),
+    ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+    ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+])
 
 def get_filename():
     dids = os.getenv("DIDS", None)
@@ -62,6 +68,13 @@ def get_top_10_trading_volumes(best=True):
         key=sort_func
     )[:10]
 
+def generate_table_data(top_10_trading_volumes):
+    table_top_10_trading_volumes_data = [['Symbol', 'Volume']]
+
+    for trading_volume in top_10_trading_volumes:
+        table_top_10_trading_volumes_data.append([trading_volume["symbol"], trading_volume["volume"]])
+
+    return table_top_10_trading_volumes_data
 
 
 
@@ -75,14 +88,8 @@ if __name__ == "__main__":
 
     top_10_best_trading_volumes = get_top_10_trading_volumes(best=True)
     top_10_worst_trading_volumes = get_top_10_trading_volumes(best=False)
-    table_top_10_best_trading_volumes_data = [['Symbol', 'Volume']]
-    table_top_10_worst_trading_volumes_data = [['Symbol', 'Volume']]
-
-    for best_trading_volume in top_10_best_trading_volumes:
-        table_top_10_best_trading_volumes_data.append([best_trading_volume["symbol"], best_trading_volume["volume"]])
-    
-    for worst_trading_volume in top_10_worst_trading_volumes:
-        table_top_10_worst_trading_volumes_data.append([worst_trading_volume["symbol"], worst_trading_volume["volume"]])
+    table_top_10_best_trading_volumes_data = generate_table_data(top_10_best_trading_volumes)
+    table_top_10_worst_trading_volumes_data = generate_table_data(top_10_worst_trading_volumes)
 
     plt.figure(figsize=(8, 4))
     plt.bar(categories, values)
@@ -95,7 +102,7 @@ if __name__ == "__main__":
     plt.savefig('bar_chart.png')
 
     # Create a PDF document
-    pdf_filename = 'report.pdf'
+    pdf_filename = '/data/outputs/report.pdf'
     c = canvas.Canvas(pdf_filename, pagesize=letter)
     c.setFillColor(colors.grey)
     c.setFont("Helvetica-Bold", 24)
@@ -108,15 +115,16 @@ if __name__ == "__main__":
 
     c.drawString(50, 270, "Top 10 Best Trading Volumes")
     table_top_10_best_trading_volumes = Table(table_top_10_best_trading_volumes_data)
+    table_top_10_best_trading_volumes.setStyle(LIST_TABLE_STYLE)
     table_top_10_best_trading_volumes.wrapOn(c, 200, 350)
     table_top_10_best_trading_volumes.drawOn(c, 150, 20)
-
     
     c.showPage()
     c.setFillColor(colors.grey)
     c.setFont("Helvetica-Bold", 14)
     c.drawString(50, 760, "Top 10 Worst Trading Volumes")
     table_top_10_worst_trading_volumes = Table(table_top_10_worst_trading_volumes_data)
+    table_top_10_worst_trading_volumes.setStyle(LIST_TABLE_STYLE)
     table_top_10_worst_trading_volumes.wrapOn(c, 200, 350)
     table_top_10_worst_trading_volumes.drawOn(c, 170, 475)
 
@@ -124,6 +132,4 @@ if __name__ == "__main__":
     print("PDF report generated successfully.")
 
     os.remove('bar_chart.png')
-    os.rename("report.pdf", "/data/outputs/report.pdf")
     print("PDF report moved to /data/outputs.")
-
