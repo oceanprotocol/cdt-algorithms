6f67b73401b0082005032de7a2708fb6a3e10ae4
Ana Loznianu <ana@oceanprotocol.com>
2023-12-06 11:57:52 +0200

    Removed comment

--- print_contents/print_contents.py
+++ print_contents/print_contents.py
@@ -5,10 +5,6 @@ import json
 def printFilesAndDirs(walk_dir):
     print('walk_dir = ' + walk_dir)
 
-    # If your current working directory may change during script execution, it's recommended to
-    # immediately convert program arguments to an absolute path. Then the variable root below will
-    # be an absolute path as well. Example:
-    # walk_dir = os.path.abspath(walk_dir)
     print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
 
     for root, subdirs, files in os.walk(walk_dir):
